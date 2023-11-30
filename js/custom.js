e33ea79966aeb4f4dcb2bb837d50705cbc6a6f2a
GuelaguetzaChurreria <148132530+GuelaguetzaChurreria@users.noreply.github.com>
2023-11-29 18:10:30 -0600

    Update custom.js

--- js/custom.js
+++ js/custom.js
@@ -18,7 +18,7 @@
     // ABOUT SLIDER
     $('body').vegas({
         slides: [
-            { src: 'images/slide-image01.jpg' },
+            { src: 'images/we.jpeg' },
             { src: 'images/slide-image02.jpg' }
         ],
         timer: false,
